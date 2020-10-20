ab25b5d46a241a61e47b962f75250d384d3860d1
Sierra Wallace <sskwallace@gmail.com>
2020-10-20 23:55:46 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -26,7 +26,7 @@ class Board
     @cells[position.to_i-1] != " "
   end
   def valid_move?(user_input)
-    user_input.to_i.between?(1, 9) && taken?(user_input) == true
+    user_input.to_i.between?(1, 9) && taken?(user_input) == false
   end
   def update(position, player)
     cells[position.to_i - 1] = player.token
